4b7ed903150da82eaa763951fd213698bae51abe
prafullarawat <prafullasrawat@gmail.com>
2022-09-10 21:36:40 +0530

    Update LoginResto.vue

--- Resto_Project/restpro/src/components/LoginResto.vue
+++ Resto_Project/restpro/src/components/LoginResto.vue
@@ -1,5 +1,5 @@
 <template>
-  <h4>Welcome To Login</h4>
+  <h4>Welcome...!!</h4>
 
   <div class="login">
     <input type="text" v-model="email" placeholder="Enter_Email" />
@@ -36,7 +36,7 @@ export default {
       }
       //console.log(result);
       else {
-        alert("Please Enter Right Credintials");
+        alert("Plz Enter Right Credintials");
       }
     },
   },
